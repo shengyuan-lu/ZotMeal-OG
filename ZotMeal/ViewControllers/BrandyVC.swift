2863c91e2e5d3398a063065ec8e17241724fb820
Shengyuan Lu <Shengyuan.lu.developer@gmail.com>
2021-01-29 22:34:22 -0800

    Icon

--- ZotMeal/ViewControllers/BrandyVC.swift
+++ ZotMeal/ViewControllers/BrandyVC.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class BrandyVC: UIViewController {
-
+    
+    // IBOutlet
+    @IBOutlet weak var foodTableView: UITableView!
+    
+    // DataStructure
+    var foodArray:[Food] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+}
 
+extension BrandyVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FoodCell") as! FoodCell
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
 }
 
